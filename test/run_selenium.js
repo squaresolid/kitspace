f87861a9087d4db4c3ee108705ebaa3652a22e49
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-27 19:11:11 +0000

    Refactor run_selenium a bit

--- test/run_selenium.js
+++ test/run_selenium.js
@@ -6,17 +6,19 @@ const accessKey = process.env.SAUCE_ACCESS_KEY
 
 const driver = new webdriver.Builder()
   .withCapabilities({
-    'browserName': 'chrome',
-    'platform': 'Windows 10',
-    'version': '55.0',
-    'username': username,
-    'accessKey': accessKey
-  }).
-  usingServer("https://" + username + ":" + accessKey +
-              "@ondemand.saucelabs.com/wd/hub")
+      'browserName': 'chrome',
+      'platform': 'Windows 10',
+      'version': '55.0',
+      'username': username,
+      'accessKey': accessKey
+  })
+  .usingServer(`https://${username}:${accessKey}@ondemand.saucelabs.com/wd/hub`)
   .build()
 
-const url = branch === 'master' ? 'https://kitnic.it' : `http://${branch}.preview.kitnic.it/`
+let url = `http://${branch}.preview.kitnic.it/`
+if (branch === 'master') {
+    url = 'https://kitnic.it'
+}
 
 driver.get(url);
 
