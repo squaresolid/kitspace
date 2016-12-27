372581dad10435526db2ed004b02a7304b5147c6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-27 19:59:15 +0000

    Start testing more versions

--- test/run_selenium.js
+++ test/run_selenium.js
@@ -4,26 +4,30 @@ const branch    = process.env.TRAVIS_BRANCH
 const username  = process.env.SAUCE_USER_NAME
 const accessKey = process.env.SAUCE_ACCESS_KEY
 
-const driver = new webdriver.Builder()
-  .withCapabilities({
-      'browserName': 'chrome',
-      'platform': 'Windows 10',
-      'version': '50.0',
-      'username': username,
-      'accessKey': accessKey
-  })
-  .usingServer(`https://${username}:${accessKey}@ondemand.saucelabs.com/wd/hub`)
-  .build()
+const chrome_versions = ['51', '52', '53', '54', '55', 'latest']
 
-let url = `http://${branch}.preview.kitnic.it/`
-if (branch === 'master') {
-    url = 'https://kitnic.it'
-}
+chrome_versions.forEach(version => {
+  const driver = new webdriver.Builder()
+    .withCapabilities({
+        browserName: 'chrome',
+        platform: 'Windows 10',
+        version,
+        username,
+        accessKey
+    })
+    .usingServer(`https://${username}:${accessKey}@ondemand.saucelabs.com/wd/hub`)
+    .build()
 
-driver.get(url);
+  let url = `http://${branch}.preview.kitnic.it/`
+  if (branch === 'master') {
+      url = 'https://kitnic.it'
+  }
 
-driver.getTitle().then(function (title) {
-    console.log("title is: " + title);
-});
+  driver.get(url);
 
-driver.quit();
+  driver.getTitle().then(function (title) {
+      console.log("title is: " + title);
+  });
+
+  driver.quit();
+})
