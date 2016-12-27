a2bbf9b4aec6e02edcc682e9dbc648cf9211c928
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-27 20:10:29 +0000

    Promisify run_selenium

--- test/run_selenium.js
+++ test/run_selenium.js
@@ -4,10 +4,11 @@ const branch    = process.env.TRAVIS_BRANCH
 const username  = process.env.SAUCE_USER_NAME
 const accessKey = process.env.SAUCE_ACCESS_KEY
 
-const chrome_versions = ['51', '52', '53', '54', '55', 'latest']
+const chrome_versions = ['50', '52', '53', '54', '55', 'latest']
 
-chrome_versions.forEach(version => {
-  const driver = new webdriver.Builder()
+const promises = chrome_versions.map(version => {
+  return new Promise((resolve, reject) => {
+    const driver = new webdriver.Builder()
     .withCapabilities({
         browserName: 'chrome',
         platform: 'Windows 10',
@@ -17,17 +18,24 @@ chrome_versions.forEach(version => {
     })
     .usingServer(`https://${username}:${accessKey}@ondemand.saucelabs.com/wd/hub`)
     .build()
+    resolve(driver)
+  }).then(driver => {
+    let url = `http://${branch}.preview.kitnic.it/`
+    if (branch === 'master') {
+        url = 'https://kitnic.it'
+    }
 
-  let url = `http://${branch}.preview.kitnic.it/`
-  if (branch === 'master') {
-      url = 'https://kitnic.it'
-  }
+    driver.get(url)
 
-  driver.get(url);
+    driver.getTitle().then(function (title) {
+        console.log("title is: " + title)
+    })
 
-  driver.getTitle().then(function (title) {
-      console.log("title is: " + title);
-  });
+    driver.quit()
+  })
+})
 
-  driver.quit();
+Promise.all(promises).catch(err => {
+    console.error(err)
+    throw err
 })
