bd6873b090063e260a5e72207fed7a4104e90fea
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-18 21:51:18 +0000

    Tru without -device queries

--- src/media_queries.js
+++ src/media_queries.js
@@ -1,14 +1,14 @@
 //these are also defined in common.scss and need to be adjusted there and here
 const mobile = `only screen
-  and (min-device-width: 320px)
-  and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 799px)
+  and (min-width: 320px)
+  and (max-width: 480px)
+  , (orientation: landscape) and (max-height: 799px)
   , (handheld)
 `
 const mobile_or_small_width = `only screen
-  and (min-device-width: 320px)
-  and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 799px)
+  and (min-width: 320px)
+  and (max-width: 480px)
+  , (orientation: landscape) and (max-height: 799px)
   , (handheld)
   , (max-width: 810px)
 `
