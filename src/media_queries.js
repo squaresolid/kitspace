932d60aae343bab050f787b787a56cfec1acf14b
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-01-18 22:10:06 +0000

    Reduce media max device media query

--- src/media_queries.js
+++ src/media_queries.js
@@ -2,14 +2,12 @@
 const mobile = `only screen
   and (min-device-width: 320px)
   and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 810px)
-  , (handheld)
+  , (orientation: landscape) and (max-device-width: 790px)
 `
 const mobile_or_small_width = `only screen
   and (min-device-width: 320px)
   and (max-device-width: 480px)
-  , (orientation: landscape) and (max-device-width: 810px)
-  , (handheld)
+  , (orientation: landscape) and (max-device-width: 790px)
   , (max-width: 810px)
 `
 const small_width = '(max-width: 810px)'
