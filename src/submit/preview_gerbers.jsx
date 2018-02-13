2f8de12fb5494f534b41ade5f30e2ee855bd6c70
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-02-13 14:55:16 +0000

    Update submit gerber preview text

--- src/submit/preview_gerbers.jsx
+++ src/submit/preview_gerbers.jsx
@@ -16,9 +16,16 @@ publicly accessible Git repository (you could use [GitHub](https://github.com)
 or [GitLab](https://gitlab.com) for instance). Preview your board by
 entering the repository URL below.
 
-If you run into any problems please get in touch via [Riot.im
-chat](https://riot.im/app/#/room/#kitnic:matrix.org), IRC (freenode#kitnic) or the
-[mailing-list](https://groups.google.com/forum/#!forum/kitnic-discuss).
+If you think it hasn't found the right gerbers you can try adding a
+[\`kitnic.yaml\`](https://github.com/monostable/kitspace#kitnicyaml-format) with the
+gerber field.
+
+    gerbers: path/to/your/gerber-folder
+
+If you run into any other problems please get in touch via
+[Riot.im chat](https://riot.im/app/#/room/#kitspace:matrix.org)
+, IRC (freenode#kitspace) or the
+[mailing-list](https://groups.google.com/forum/#!forum/kitspace).
 You can also use the [Tracespace Viewer](http://viewer.tracespace.io) to try
 and figure out any issues with the board rendering.
 
